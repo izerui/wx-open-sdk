6d52464afc6860e5be10de14a9618948e2d8856f
serv <liuyuhua69@gmail.com>
2016-04-25 16:56:40 +0800

    uploadimg

--- src/main/java/com/github/izerui/weixin/MediaService.java
+++ src/main/java/com/github/izerui/weixin/MediaService.java
@@ -28,5 +28,13 @@ public interface MediaService {
 
     URL url(String mediaId);
 
+    /**
+     * 上传图文消息内的图片获取URL
+     *
+     * 请注意，本接口所上传的图片不占用公众号的素材库中图片数量的5000个的限制。图片仅支持jpg/png格式，大小必须在1MB以下。
+     *
+     * @param media
+     * @return
+     */
     String uploadImg(Media media);
 }
