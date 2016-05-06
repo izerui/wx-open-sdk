1d7d2e90dc10096337dd1183511c6b27cbc3e2d1
serv <liuyuhua69@gmail.com>
2016-05-06 10:50:55 +0800

    注释

--- src/main/java/com/github/izerui/weixin/converter/JacksonConverter.java
+++ src/main/java/com/github/izerui/weixin/converter/JacksonConverter.java
@@ -29,11 +29,11 @@ public class JacksonConverter<REQUEST,RESPONSE> {
     }
 
     /**
-     * 将responseBody字节数组 转换成 RESPONSE对象
+     * 将responseBody字节数组 转换成 结果对象
      * @param mapper
-     * @param type RESPONSE 的 Type类型
+     * @param type 结果对象的 Type类型
      * @param response 请求返回的responseBody字节数组
-     * @return 返回 RESPONSE 对象
+     * @return 返回 结果对象
      * @throws IOException
      */
     public RESPONSE response(ObjectMapper mapper, Type type, byte[] response) throws IOException {
